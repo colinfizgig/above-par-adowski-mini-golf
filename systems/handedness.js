d2d1c6bcf7811969921bd9325c5ecdf564840439
Kevin Olson <kolson@paradowski.com>
2022-09-28 15:31:41 -0500

    Bit of cleanup

--- systems/handedness.js
+++ systems/handedness.js
@@ -4,8 +4,6 @@ AFRAME.registerSystem("handedness", {
   },
 
   init: function () {
-    // Called on scene initialization.
-
     this.touchControllerR = document.querySelector(
       `[oculus-touch-controls="hand:right;model:false;"]`
     );
@@ -49,35 +47,37 @@ AFRAME.registerSystem("handedness", {
     el.removeAttribute("raycaster");
   },
 
-  setHand(hand) {
-    console.log("Change Handedness", hand);
+  updateClubVisuals(hand) {
+    const leftController = document.querySelector("#left-controller");
+    const rightController = document.querySelector("#right-controller");
+    const clubR = document.querySelector("#club-right");
+    const clubL = document.querySelector("#club-left");
 
+    if (hand === "right") {
+      leftController.setAttribute("visible", "false");
+      rightController.setAttribute("visible", "true");
+      this.removePhysics(clubL);
+      this.addPhysics(clubR);
+      document
+        .querySelector(".floor")
+        .setAttribute("ground-listener", "handedness: right;");
+    } else if (hand === "left") {
+      leftController.setAttribute("visible", "true");
+      rightController.setAttribute("visible", "false");
+      this.removePhysics(clubR);
+      this.addPhysics(clubL);
+      document
+        .querySelector(".floor")
+        .setAttribute("ground-listener", "handedness: left;");
+    }
+  },
+
+  setHand(hand) {
     // Only change hands if hand isn't correct
     if (hand != this.data.hand) {
       this.data.hand = hand;
 
-      const leftController = document.querySelector("#left-controller");
-      const rightController = document.querySelector("#right-controller");
-      const clubR = document.querySelector("#club-right");
-      const clubL = document.querySelector("#club-left");
-
-      if (hand === "right") {
-        leftController.setAttribute("visible", "false");
-        rightController.setAttribute("visible", "true");
-        this.removePhysics(clubL);
-        this.addPhysics(clubR);
-        document
-          .querySelector(".floor")
-          .setAttribute("ground-listener", "handedness: right;");
-      } else if (hand === "left") {
-        leftController.setAttribute("visible", "true");
-        rightController.setAttribute("visible", "false");
-        this.removePhysics(clubR);
-        this.addPhysics(clubL);
-        document
-          .querySelector(".floor")
-          .setAttribute("ground-listener", "handedness: left;");
-      }
+      this.updateClubVisuals(hand);
 
       document.dispatchEvent(
         new CustomEvent("handedness-changed", { detail: { hand: hand } })
