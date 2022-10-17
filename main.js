f2e3ae33f98940562490aa12dd724dfaa5da14a0
Irina Fawcett <ifawcett@IrinaFaCreative.attlocal.net>
2022-10-17 16:55:36 -0500

    Removed laser controls and temporarily turned off tutorial

--- main.js
+++ main.js
@@ -41,10 +41,10 @@ AFRAME.registerComponent("ground-listener", {
     });
 
     const head = document.querySelector("#head");
-    head.setAttribute("tutorial", "");
-    document.addEventListener("remove-tutorial", () => {
-      head.removeAttribute("tutorial");
-    });
+    // head.setAttribute("tutorial", "");
+    // document.addEventListener("remove-tutorial", () => {
+    //   head.removeAttribute("tutorial");
+    // });
   },
 
   tick: function () {
