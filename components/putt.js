1414406bfbd4b0a56484efe0da98b18953047fda
Ethan Michalicek <emichalicek@paradowski.com>
2022-10-27 11:31:18 -0500

    Fixed issue with trying to get global RAF

--- components/putt.js
+++ components/putt.js
@@ -479,7 +479,7 @@ AFRAME.registerComponent("putt", {
   globalRAF(callback) {
     const xrSession = !!this.el.sceneEl.renderer.xr?.getSession;
     if (!xrSession) return window.requestAnimationFrame(callback);
-    return this.el.sceneEl.renderer.xr?.getSession().requestAnimationFrame(callback);
+    return this.el.sceneEl.renderer.xr?.getSession()?.requestAnimationFrame(callback);
   },
 
   newBall: function (newballposition) {
